90e1e36b46e17a3abf09568e168f3aebff13e18c
jim <liu1084@163.com>
2016-10-28 18:37:17 +0800

    update: add php markdown

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,6 @@ elixir(mix => {
 	mix.webpack('app.js')
 		//.webpack('home.js')
 		.copy('resources/assets/js/home.js', 'public/home.js')
-		//.copy('resources/assets/bower_components', 'public/bower_components')
+		.copy('resources/assets/bower_components', 'public/bower_components')
 		.webpack('article/create.js');
 });
